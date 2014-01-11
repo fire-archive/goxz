8e6f1098a1d9d88cb4ad327a2bf99030a11a677b
rawr <rawr@localhost>
2014-01-10 18:54:52 -0800

    Add trivial example program using the library

---
+++ cmd/xzpipe.go
@@ -0,0 +1,43 @@
+// Copyright 2012 Joe Tsai. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package main
+
+import "os"
+import "io"
+import "flag"
+import "bitbucket.org/rawr/goxz/xz"
+
+func errPanic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	// Switch compression mode.
+	var dec bool
+	fs := flag.NewFlagSet("xzpipe", flag.ExitOnError)
+	fs.BoolVar(&dec, "d", false, "Decompression mode")
+	fs.Parse(os.Args[1:])
+
+	if !dec {
+		// Compression mode.
+		xz, err := xz.NewWriter(os.Stdout)
+		errPanic(err)
+		defer xz.Close()
+
+		// Copy all stdin to the compressor.
+		_, err = io.Copy(xz, os.Stdin)
+		errPanic(err)
+	} else {
+		// Decompression mode.
+		xz, err := xz.NewReader(os.Stdin)
+		errPanic(err)
+		defer xz.Close()
+
+		// Copy all decompressor output to stdout.
+		_, err = io.Copy(os.Stdout, xz)
+		errPanic(err)
+	}
+}
