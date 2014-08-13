d8f8db76da7cdb119e1b1a972e24798076d23fc8
rawr <rawr@localhost>
2014-08-13 01:59:15 -0700

    Trivial comment change

--- lzma/const.go
+++ lzma/const.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE.md file.
 
-// Package lzma implements reading/writing of files in the LZMA_ALONE format.
+// Package lzma implements reading and writing files in the LZMA_ALONE format.
 // This file format is deprecated. As such, consider using the XZ format.
 package lzma
 
