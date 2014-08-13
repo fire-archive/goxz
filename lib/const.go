75636209322ca8e9da781102f6bf86b31fbf2848
rawr <rawr@localhost>
2014-08-13 13:21:58 -0700

    Update constants

--- lib/const.go
+++ lib/const.go
@@ -15,38 +15,41 @@ package lib
 /*
 #cgo LDFLAGS: -llzma
 #include "lzma.h"
-
-// HACK(jtsai): LZMA_VLI_UNKNOWN is effectively -1 which overflows when Go tries
-//  to cast the constant to the lzma_vli type which is unsigned. Do the casting
-//  in C and then use the value in Go.
-const lzma_vli _LZMA_VLI_UNKNOWN = LZMA_VLI_UNKNOWN;
 */
 import "C"
 
-// Status code constants.
-const OK = C.LZMA_OK
-const STREAM_END = C.LZMA_STREAM_END
-const NO_CHECK = C.LZMA_NO_CHECK
-const UNSUPPORTED_CHECK = C.LZMA_UNSUPPORTED_CHECK
-const GET_CHECK = C.LZMA_GET_CHECK
-const MEM_ERROR = C.LZMA_MEM_ERROR
-const MEMLIMIT_ERROR = C.LZMA_MEMLIMIT_ERROR
-const FORMAT_ERROR = C.LZMA_FORMAT_ERROR
-const OPTIONS_ERROR = C.LZMA_OPTIONS_ERROR
-const DATA_ERROR = C.LZMA_DATA_ERROR
-const BUF_ERROR = C.LZMA_BUF_ERROR
-const PROG_ERROR = C.LZMA_PROG_ERROR
+import "math"
 
-// Flushing mode constants.
-const RUN = C.LZMA_RUN
-const SYNC_FLUSH = C.LZMA_SYNC_FLUSH
-const FULL_FLUSH = C.LZMA_FULL_FLUSH
-const FINISH = C.LZMA_FINISH
+// Status code constants:
+const (
+	OK                = C.LZMA_OK
+	STREAM_END        = C.LZMA_STREAM_END
+	NO_CHECK          = C.LZMA_NO_CHECK
+	UNSUPPORTED_CHECK = C.LZMA_UNSUPPORTED_CHECK
+	GET_CHECK         = C.LZMA_GET_CHECK
+	MEM_ERROR         = C.LZMA_MEM_ERROR
+	MEMLIMIT_ERROR    = C.LZMA_MEMLIMIT_ERROR
+	FORMAT_ERROR      = C.LZMA_FORMAT_ERROR
+	OPTIONS_ERROR     = C.LZMA_OPTIONS_ERROR
+	DATA_ERROR        = C.LZMA_DATA_ERROR
+	BUF_ERROR         = C.LZMA_BUF_ERROR
+	PROG_ERROR        = C.LZMA_PROG_ERROR
+)
 
-// Encoder flag constants.
-const PRESET_DEFAULT = C.LZMA_PRESET_DEFAULT
-const PRESET_LEVEL_MASK = C.LZMA_PRESET_LEVEL_MASK
-const PRESET_EXTREME = C.LZMA_PRESET_EXTREME
+// Flushing mode constants:
+const (
+	RUN        = C.LZMA_RUN
+	SYNC_FLUSH = C.LZMA_SYNC_FLUSH
+	FULL_FLUSH = C.LZMA_FULL_FLUSH
+	FINISH     = C.LZMA_FINISH
+)
+
+// Encoder flag constants:
+const (
+	PRESET_DEFAULT    = C.LZMA_PRESET_DEFAULT
+	PRESET_LEVEL_MASK = C.LZMA_PRESET_LEVEL_MASK
+	PRESET_EXTREME    = C.LZMA_PRESET_EXTREME
+)
 
 const (
 	PRESET_LEVEL0 = iota
@@ -61,36 +64,66 @@ const (
 	PRESET_LEVEL9
 )
 
-// Decoder flag constants.
-const TELL_NO_CHECK = C.LZMA_TELL_NO_CHECK
-const TELL_UNSUPPORTED_CHECK = C.LZMA_TELL_UNSUPPORTED_CHECK
-const TELL_ANY_CHECK = C.LZMA_TELL_ANY_CHECK
-const CONCATENATED = C.LZMA_CONCATENATED
+// Decoder flag constants:
+const (
+	TELL_NO_CHECK          = C.LZMA_TELL_NO_CHECK
+	TELL_UNSUPPORTED_CHECK = C.LZMA_TELL_UNSUPPORTED_CHECK
+	TELL_ANY_CHECK         = C.LZMA_TELL_ANY_CHECK
+	CONCATENATED           = C.LZMA_CONCATENATED
+)
 
-// Integrity check constants.
-const CHECK_NONE = C.LZMA_CHECK_NONE
-const CHECK_CRC32 = C.LZMA_CHECK_CRC32
-const CHECK_CRC64 = C.LZMA_CHECK_CRC64
-const CHECK_SHA256 = C.LZMA_CHECK_SHA256
+// Integrity check constants:
+const (
+	CHECK_NONE   = C.LZMA_CHECK_NONE
+	CHECK_CRC32  = C.LZMA_CHECK_CRC32
+	CHECK_CRC64  = C.LZMA_CHECK_CRC64
+	CHECK_SHA256 = C.LZMA_CHECK_SHA256
+)
 
-// Index iteration mode constants.
-const INDEX_ITER_ANY = C.LZMA_INDEX_ITER_ANY
-const INDEX_ITER_BLOCK = C.LZMA_INDEX_ITER_BLOCK
-const INDEX_ITER_NONEMPTY_BLOCK = C.LZMA_INDEX_ITER_NONEMPTY_BLOCK
-const INDEX_ITER_STREAM = C.LZMA_INDEX_ITER_STREAM
+// Index iteration mode constants:
+const (
+	INDEX_ITER_ANY            = C.LZMA_INDEX_ITER_ANY
+	INDEX_ITER_BLOCK          = C.LZMA_INDEX_ITER_BLOCK
+	INDEX_ITER_NONEMPTY_BLOCK = C.LZMA_INDEX_ITER_NONEMPTY_BLOCK
+	INDEX_ITER_STREAM         = C.LZMA_INDEX_ITER_STREAM
+)
 
-// Variable length integer constants.
-var VLI_UNKNOWN = C._LZMA_VLI_UNKNOWN
+// Variable length integer constants:
+const (
+	VLI_MAX       = C.LZMA_VLI_MAX
+	VLI_BYTES_MAX = C.LZMA_VLI_BYTES_MAX
+	VLI_UNKNOWN   = math.MaxUint64
+)
 
-const VLI_MAX = C.LZMA_VLI_MAX
-const VLI_BYTES_MAX = C.LZMA_VLI_BYTES_MAX
+// HACK(jtsai): LZMA_VLI_UNKNOWN is effectively -1 which overflows when Go tries
+//  to cast the constant to the lzma_vli type which is unsigned. Since the
+//  lzma_vli type is a uint64_t, then the VLI_UNKNOWN constant essentially gets
+//  casted as MaxUint64.
 
-// File format constants.
-var HEADER_MAGIC = []byte{0xFD, '7', 'z', 'X', 'Z', 0x00}
-var FOOTER_MAGIC = []byte{'Y', 'Z'}
-const STREAM_HEADER_SIZE = C.LZMA_STREAM_HEADER_SIZE
-const STREAM_FOOTER_SIZE = C.LZMA_STREAM_HEADER_SIZE
-const BACKWARD_SIZE_MAX = C.LZMA_BACKWARD_SIZE_MAX
-const BACKWARD_SIZE_MIN = C.LZMA_BACKWARD_SIZE_MIN
-const BLOCK_HEADER_SIZE_MAX = C.LZMA_BLOCK_HEADER_SIZE_MAX
-const BLOCK_HEADER_SIZE_MIN = C.LZMA_BLOCK_HEADER_SIZE_MIN
+// Filter identification consts:
+const (
+	FILTER_LZMA1    = C.LZMA_FILTER_LZMA1
+	FILTER_LZMA2    = C.LZMA_FILTER_LZMA2
+	FILTER_DELTA    = C.LZMA_FILTER_DELTA
+	FILTER_X86      = C.LZMA_FILTER_X86
+	FILTER_POWERPC  = C.LZMA_FILTER_POWERPC
+	FILTER_IA64     = C.LZMA_FILTER_IA64
+	FILTER_ARM      = C.LZMA_FILTER_ARM
+	FILTER_ARMTHUMB = C.LZMA_FILTER_ARMTHUMB
+	FILTER_SPARC    = C.LZMA_FILTER_SPARC
+)
+
+// File format constants:
+var (
+	HEADER_MAGIC = []byte{0xFD, '7', 'z', 'X', 'Z', 0x00}
+	FOOTER_MAGIC = []byte{'Y', 'Z'}
+)
+
+const (
+	STREAM_HEADER_SIZE    = C.LZMA_STREAM_HEADER_SIZE
+	STREAM_FOOTER_SIZE    = C.LZMA_STREAM_HEADER_SIZE
+	BACKWARD_SIZE_MAX     = C.LZMA_BACKWARD_SIZE_MAX
+	BACKWARD_SIZE_MIN     = C.LZMA_BACKWARD_SIZE_MIN
+	BLOCK_HEADER_SIZE_MAX = C.LZMA_BLOCK_HEADER_SIZE_MAX
+	BLOCK_HEADER_SIZE_MIN = C.LZMA_BLOCK_HEADER_SIZE_MIN
+)
---
+++ xz/const.go
@@ -0,0 +1,21 @@
+// Copyright 2014, Joe Tsai. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE.md file.
+
+// Package xz implements reading and writing files in the XZ file format.
+package xz
+
+import "bitbucket.org/rawr/goxz/lib"
+
+// These constants are copied from the lib package, so that code that imports
+// "goxz/xz" does not also have to import "goxz/lib".
+const (
+  Extreme            = lib.PRESET_EXTREME
+  BestSpeed          = lib.PRESET_LEVEL0
+  BestCompression    = lib.PRESET_LEVEL9
+  DefaultCompression = lib.PRESET_DEFAULT
+)
+
+// Chunksize configruation constants:
+const CHUNKSIZE_INFINITE = -1
+const CHUNKSIZE_DEFAULT = 1 << 20
