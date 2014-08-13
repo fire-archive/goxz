2d757f3e44c9d8a53eec6dcf3d5e17515ccbdf37
rawr <rawr@localhost>
2014-08-13 13:40:38 -0700

    Add constants for package xz

--- xz/const.go
+++ xz/const.go
@@ -10,10 +10,10 @@ import "bitbucket.org/rawr/goxz/lib"
 // These constants are copied from the lib package, so that code that imports
 // "goxz/xz" does not also have to import "goxz/lib".
 const (
-  Extreme            = lib.PRESET_EXTREME
-  BestSpeed          = lib.PRESET_LEVEL0
-  BestCompression    = lib.PRESET_LEVEL9
-  DefaultCompression = lib.PRESET_DEFAULT
+	Extreme            = lib.PRESET_EXTREME
+	BestSpeed          = lib.PRESET_LEVEL0
+	BestCompression    = lib.PRESET_LEVEL9
+	DefaultCompression = lib.PRESET_DEFAULT
 )
 
 // Chunksize configruation constants:
