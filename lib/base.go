70637fd42c7d7cb74f85bc107ae313b32e8fa172
rawr <rawr@localhost>
2014-08-30 10:05:51 -0700

    Make the liblzma wrapper package use the new BufferPipe

--- lib/base.go
+++ lib/base.go
@@ -15,11 +15,17 @@ void lzma_stream_init_(lzma_stream* strm) {
 */
 import "C"
 
+import "io"
 import "runtime"
+import "bitbucket.org/rawr/golib/bufpipe"
+
+const chunkSize = 1 << 16
 
 type Stream struct {
 	stream C.lzma_stream
 	refs   []interface{} // Pointer reference to any struct to help Go's GC
+	inBuf  *bufpipe.BufferPipe
+	outBuf *bufpipe.BufferPipe
 }
 
 func (z *Stream) C() *C.lzma_stream {
@@ -37,17 +43,17 @@ func (z *Stream) appendRef(ref interface{}) {
 	z.refs = append(z.refs, ref)
 }
 
-func (z *Stream) CodeSlice(action int, input []byte, output []byte) (error, int, int) {
-	z.SetInput(input)
-	z.SetOutput(output)
-	err := z.Code(action)
-	cntIn := len(input) - int(z.stream.avail_in)
-	cntOut := len(output) - int(z.stream.avail_out)
-	return err, cntIn, cntOut
-}
-
 func (z *Stream) Code(action int) error {
 	return NewError(C.lzma_code(z.C(), C.lzma_action(action)))
+}
+
+func (z *Stream) CodeSlice(action int, dst []byte, src []byte) (int, int, error) {
+	z.SetOutput(dst)
+	z.SetInput(src)
+	err := z.Code(action)
+	wrCnt := len(dst) - int(z.stream.avail_out)
+	rdCnt := len(src) - int(z.stream.avail_in)
+	return wrCnt, rdCnt, err
 }
 
 // It is not required that End() be called since NewStream() sets a Go finalizer
@@ -78,4 +84,104 @@ func (z *Stream) GetAvailOut() int64 {
 
 func (z *Stream) GetTotalOut() int64 {
 	return int64(z.stream.total_out)
+}
+
+func (z *Stream) GetBuffers() (outBuf, inBuf *bufpipe.BufferPipe) {
+	return z.outBuf, z.inBuf
+}
+
+func (z *Stream) SetBuffers(outBuf, inBuf *bufpipe.BufferPipe) {
+	if outBuf != nil && outBuf.GetMode() != bufpipe.RingPoll {
+		panic("internal buffers must be in polling mode")
+	}
+	if inBuf != nil && inBuf.GetMode() != bufpipe.RingPoll {
+		panic("internal buffers must be in polling mode")
+	}
+	z.outBuf, z.inBuf = outBuf, inBuf
+}
+
+func (z *Stream) Process(code int, dst io.Writer, src io.Reader) (outCnt, inCnt int64, err error) {
+	if src == nil && dst == nil {
+		return
+	}
+
+	// Lazy allocate internal ring buffer.
+	if src != nil && z.inBuf == nil {
+		buf := make([]byte, chunkSize)
+		z.inBuf = bufpipe.NewBufferPipe(buf, bufpipe.RingPoll)
+	}
+	if dst != nil && z.outBuf == nil {
+		buf := make([]byte, chunkSize)
+		z.outBuf = bufpipe.NewBufferPipe(buf, bufpipe.RingPoll)
+	}
+
+	var rdCnt, wrCnt int
+	var rdErr, wrErr, xzErr error
+	for {
+		for rdErr == nil && src != nil && z.inBuf.Length() == 0 {
+			buf, _ := z.inBuf.WriteSlice()
+			rdCnt, rdErr = src.Read(buf)
+			z.inBuf.WriteMark(rdCnt)
+			inCnt += int64(rdCnt)
+		}
+
+		inBuf, _ := z.inBuf.ReadSlice()
+		outBuf, _ := z.outBuf.WriteSlice()
+		wrCnt, rdCnt, xzErr = z.CodeSlice(code, outBuf, inBuf)
+		z.inBuf.ReadMark(rdCnt)
+		z.outBuf.WriteMark(wrCnt)
+		if (src == nil || len(inBuf) > 0) && (dst == nil || len(outBuf) > 0) {
+			if xzErr == Error(BUF_ERROR) {
+				xzErr = nil // Temporary error, just feed more!
+			}
+		}
+
+		for wrErr == nil && dst != nil && z.outBuf.Length() > 0 {
+			buf, _ := z.outBuf.ReadSlice()
+			wrCnt, wrErr = dst.Write(buf)
+			z.outBuf.ReadMark(wrCnt)
+			outCnt += int64(wrCnt)
+		}
+
+		switch {
+		case wrErr != nil:
+			return outCnt, inCnt, wrErr
+		case xzErr != nil && z.outBuf.Length() == 0:
+			return outCnt, inCnt, xzErr
+		case rdErr != nil && z.outBuf.Length() == 0 && z.inBuf.Length() == 0:
+			return outCnt, inCnt, rdErr
+		}
+	}
+}
+
+func (z *Stream) ProcessPipe(code int, dst, src *bufpipe.BufferPipe) (outCnt, inCnt int64, err error) {
+	if src == nil && dst == nil {
+		return
+	}
+
+	var inBuf, outBuf []byte
+	for {
+		if src != nil {
+			if inBuf, err = src.ReadSlice(); err != nil {
+				return outCnt, inCnt, err
+			}
+		}
+
+		if dst != nil {
+			if outBuf, err = dst.WriteSlice(); err != nil {
+				return outCnt, inCnt, err
+			}
+		}
+
+		wrCnt, rdCnt, err := z.CodeSlice(code, outBuf, inBuf)
+		src.ReadMark(rdCnt)
+		dst.WriteMark(wrCnt)
+
+		inCnt += int64(rdCnt)
+		outCnt += int64(wrCnt)
+
+		if err != nil {
+			return outCnt, inCnt, err
+		}
+	}
 }
